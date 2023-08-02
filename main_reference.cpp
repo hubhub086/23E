d1cb158e38108d551270e36313a37b7d0252157c
SongLiang Cao <2023733547@qq.com>
2023-08-02 09:03:39 +0800

    first push

---
+++ main_reference.cpp
@@ -0,0 +1,443 @@
+#include <iostream>
+#include <fstream>
+#include <pthread.h>
+#include <time.h>
+#include <unistd.h>
+#include <NvInfer.h>
+#include <cuda_runtime_api.h>
+#include "opencv2/core.hpp"
+#include "opencv2/opencv.hpp"
+
+#include "uart.h"
+
+using namespace std;
+using namespace cv;
+
+void getContours(Mat imgDil, Mat* img);
+vector<Point> get_blobs_center(Mat img, int min_area, int max_area);
+Point get_center_position(Mat* img_origin, Rect roi);
+vector<Point> get_map_pos(Mat* img);
+
+Mat frame;
+Mat gray, hsv_frame, orange_thres, green_thres, black_thres;
+Mat img_warp_hsv,img_wrap_BGR;
+vector<Mat> hsvSplit;
+
+vector<Rect> location_rois = {{277,5,60,60},
+							  {0,0,60,60},
+							  {0,0,60,60},
+							  {0,0,60,60}};
+
+int fd;
+int main_status = 10;  // modified in uart.cpp --> void* receive_thread(void* ptr)
+int moving_mode = -1;  // modified in uart.cpp --> void* receive_thread(void* ptr)
+vector<Point2f> dst_points = {Point2f(0.0, 0.0),
+							  Point2f(0.0, 500.0),
+				              Point2f(400.0, 0.0),
+				              Point2f(400.0, 500.0)};
+vector<Point2f> map_pos_2f;
+vector<Point> black_blob_pos, orange_blob_pos;
+Point paw_zero_position;
+Point paw_outside_position;
+// = {-200, -200}
+Rect targetRect = {0,0,155,160};  //mask roi for blackHome
+string msg;
+// [247, 92;
+//  508, 84;
+//  258, 415;
+//  517, 409]
+
+int main()
+{
+    cout << "###  item_sorting_system  ###" << endl;
+    VideoCapture capture(1, CAP_V4L2);
+	if (!capture.isOpened())
+	{
+		cout << "camera not open!" << endl;
+		exit(1);
+	}
+    // camera config
+    capture.set(CAP_PROP_FOURCC, VideoWriter::fourcc('M', 'J', 'P', 'G'));
+    capture.set(CAP_PROP_FRAME_WIDTH,640);
+    capture.set(CAP_PROP_FRAME_HEIGHT,480);
+    capture.set(CAP_PROP_FPS,60);
+    capture.set(CAP_PROP_BUFFERSIZE, 1);
+    capture.set(CAP_PROP_AUTO_WB, 1);
+    // capture.set(CAP_PROP_WB_TEMPERATURE, 4000);
+    cout << "CAP_PROP_FPS" << capture.get(CAP_PROP_FPS) << endl;
+    cout << "CAP_PROP_BUFFERSIZE=" << capture.get(CAP_PROP_BUFFERSIZE) << endl;
+    cout << "CAP_PROP_AUTO_WB=" << capture.get(CAP_PROP_AUTO_WB) << endl;
+	cout << "CAP_PROP_WB_TEMPERATURE=" << capture.get(CAP_PROP_WB_TEMPERATURE) << endl;
+    cout << "CAP_PROP_FRAME_SIZE=(" << capture.get(CAP_PROP_FRAME_WIDTH) << "," << capture.get(CAP_PROP_FRAME_HEIGHT) << ")" << endl;
+
+    //uart init
+    fd = uart_init("/dev/ttyTHS1", 115200, 8, 'N', 1);
+    uart_receiveThread_touch();
+	uart_send(fd, "main init finish\n");
+
+    clock_t start, end;
+    while(1)
+    {   
+		cout << "### item sorting system  ###" << endl;
+        start = clock();
+        capture >> frame;
+        cvtColor(frame, hsv_frame, COLOR_BGR2HSV); 
+		split(hsv_frame, hsvSplit);	
+        equalizeHist(hsvSplit[2], hsvSplit[2]); 
+        merge(hsvSplit, hsv_frame);	
+
+		cout << main_status << endl;
+
+
+        if (main_status == 10)
+		{
+			// 识别地图角点
+			inRange(hsv_frame, Scalar(35, 43, 36), Scalar(77, 255, 255), green_thres); //绿色
+			Mat element = getStructuringElement(MORPH_RECT, Size(5, 5));
+			morphologyEx(green_thres, green_thres, MORPH_OPEN, element);  //开操作
+			morphologyEx(green_thres, green_thres, MORPH_CLOSE, element);  //闭操作
+			vector<Point> map_pos = get_map_pos(&green_thres);
+			if (map_pos.size() == 4 && main_status == 10)
+			{
+				map_pos_2f.clear();
+				for (int i=0; i<map_pos.size(); i++)
+				{
+					map_pos_2f.push_back(Point2f(map_pos[i].x, map_pos[i].y));
+				}
+				cout << map_pos << endl;
+			}
+			if (map_pos_2f.size() == 4)
+			{
+				Mat rotation,img_warp;
+				rotation=getPerspectiveTransform(map_pos_2f,dst_points);
+				warpPerspective(frame, img_warp, rotation, Size(400, 500));
+				imshow("imgwrap", img_warp);
+			}
+			imshow("green", green_thres);
+		}
+		else if (main_status == 11)
+		{
+			// 标定爪子中心点
+			Mat rotation,img_warp;
+			rotation=getPerspectiveTransform(map_pos_2f,dst_points);
+			warpPerspective(hsv_frame, img_warp, rotation, Size(400, 500));
+			warpPerspective(frame, img_wrap_BGR, rotation, Size(400, 500));
+			inRange(img_warp, Scalar(35, 43, 36), Scalar(77, 255, 255), green_thres); //绿色
+			Mat element = getStructuringElement(MORPH_RECT, Size(5, 5));
+			morphologyEx(green_thres, green_thres, MORPH_OPEN, element);  //开操作
+			morphologyEx(green_thres, green_thres, MORPH_CLOSE, element);  //闭操作
+			vector<Point> paw_pos = get_blobs_center(green_thres, 900, 4000);
+			cout << paw_pos << endl;
+			if (paw_pos.size() > 0)
+			{
+				paw_zero_position = paw_pos[0];
+				paw_zero_position.x -= 52;
+				cout << paw_zero_position << endl;
+				paw_outside_position = {-130-paw_zero_position.x, 0-paw_zero_position.y};
+			}
+			imshow("imgwrap", img_wrap_BGR);
+			imshow("green_thresh", green_thres);
+		}
+		else if (main_status >= 20)
+		{
+			if (main_status == 20)
+			{
+				msg = "tx=" + to_string(paw_outside_position.x) +".00!\r\n";
+				uart_send(fd, msg.c_str());
+				usleep(10000);
+				cout << msg << endl;
+				msg = "ty=" + to_string(paw_outside_position.y) +".00!\r\n";
+				cout << msg << endl;
+				uart_send(fd, msg.c_str());
+				usleep(10000);
+				uart_send(fd, "id=2.00!\r\n");
+				usleep(10000);
+				main_status = 21;
+				continue;
+			}
+			//识别物块
+			if (map_pos_2f.size() == 4)
+			{
+				Mat rotation;
+				rotation=getPerspectiveTransform(map_pos_2f,dst_points);
+				warpPerspective(hsv_frame, img_warp_hsv, rotation, Size(400, 500));
+				warpPerspective(frame, img_wrap_BGR, rotation, Size(400, 500));
+				imshow("imgwraphsv", img_warp_hsv);
+			}
+			inRange(img_warp_hsv, Scalar(0, 43, 56), Scalar(150, 255, 255), orange_thres); //黄色
+			inRange(img_warp_hsv, Scalar(0, 0, 0), Scalar(200, 200, 29), black_thres); //黑色
+			Mat subImage = black_thres(targetRect);
+    		subImage.setTo(0);
+			black_thres(Rect(0,0,400,12)).setTo(0);
+			black_blob_pos = get_blobs_center(black_thres, 1000, 4000);
+			orange_blob_pos = get_blobs_center(orange_thres, 1000, 4000);
+			imshow("orange_thres", orange_thres);
+			imshow("black_thres", black_thres);
+			imshow("imgwrapBGR", img_wrap_BGR);
+			if (main_status == 21)
+			{
+				//black to black
+				inRange(img_warp_hsv, Scalar(0, 0, 0), Scalar(200, 200, 29), black_thres); //黑色
+				Mat subImage = black_thres(targetRect);
+    			subImage.setTo(0);
+				black_blob_pos = get_blobs_center(black_thres, 1000, 4000);
+				if (black_blob_pos.size() > 0)
+				{
+					if (moving_mode == 0)
+					{
+						//grab
+						Point tempTarget = black_blob_pos[0];
+						msg = "tx=" + to_string(tempTarget.x-paw_zero_position.x) +".00!\r\n";
+						uart_send(fd, msg.c_str());
+						usleep(10000);
+						msg = "ty=" + to_string(tempTarget.y-paw_zero_position.y) +".00!\r\n";
+						uart_send(fd, msg.c_str());
+						usleep(10000);
+						uart_send(fd, "id=1.00!\r\n");
+						usleep(10000);
+						moving_mode = -1;
+					}
+					else if (moving_mode == 1)
+					{
+						//put down
+						msg = "tx=" + to_string(int(50.00-paw_zero_position.x)) +".00!\r\n";
+						uart_send(fd, msg.c_str());
+						msg = "ty=" + to_string(int(50.00-paw_zero_position.y)) +".00!\r\n";
+						uart_send(fd, msg.c_str());
+						uart_send(fd, "id=2.00!\r\n");
+					}
+					else if (moving_mode == 2)
+					{
+						// reset(outside)
+						msg = "tx=" + to_string(int(50.00-paw_zero_position.x)) +".00!\r\n";
+						uart_send(fd, msg.c_str());
+						msg = "ty=" + to_string(int(50.00-paw_zero_position.y)) +".00!\r\n";
+						uart_send(fd, msg.c_str());
+						uart_send(fd, "id=2.00!\r\n");
+					}
+				}
+			}
+		}
+        // //形状识别
+        // Mat imgBlur;
+        // GaussianBlur(gray, imgBlur, Size(3,3), 3, 0);
+        // Mat img_threshold;
+        // threshold(gray, img_threshold, 0, 255, THRESH_BINARY | THRESH_OTSU);
+        // Mat imgCanny;
+        // Canny(img_threshold, imgCanny, 25, 75);
+        // Mat imgDil;
+        // Mat kernel = getStructuringElement(MORPH_RECT, Size(3,3));
+        // dilate(imgCanny, imgDil, kernel);
+        // getContours(imgDil, frame);
+
+        end = clock();
+        imshow("frame", frame);
+        
+        // cout << "fps=" << 1/(double(end-start)/CLOCKS_PER_SEC) << endl;
+        int key = waitKey(1);
+        if (key == 27)
+        {
+			fstream f;
+			f.open("../map_pos.txt", ios::out);
+			f << map_pos_2f << endl;
+			f.close();
+            break;
+        }
+		else if (char(key) == 's')
+		{
+			main_status = 11;
+		}
+		else if (char(key) == 'p')
+		{
+			main_status = 20;
+		}
+    }
+
+    return 0;
+}
+
+vector<Point> get_blobs_center(Mat img, int min_area, int max_area)
+{
+	vector<Point> center_points;
+	vector<vector<Point>> contours;
+	vector<Vec4i> hierarchy;
+	findContours(img, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+	for (int i = 0; i < contours.size(); i++)
+	{
+		int area = contourArea(contours[i]);
+		if (area > min_area && area < max_area)
+		{
+			Rect bound = boundingRect(contours[i]);
+			Point center = {0,0};
+			int count = 0;
+			for (int j=0; j<bound.width; j++)
+			{
+				for (int k=0; k< bound.height; k++)
+				{
+					if (img.at<uchar>(bound.y+k, bound.x+j) == 255)
+					{
+						center += {bound.x+j, bound.y+k};
+						count++;
+					}
+				}
+			}
+			center.x /= count;
+			center.y /= count;
+			center_points.push_back(center);
+			circle(frame, center, 3, Scalar(0, 255, 120), -1);
+			circle(img_wrap_BGR, center, 5, Scalar(0, 255, 120), -1);
+			rectangle(img_wrap_BGR, bound.tl(), bound.br(), Scalar(0, 255, 0), 5);
+		}
+	}
+	return center_points;
+}
+
+vector<Point> get_map_pos(Mat* img)
+{
+	vector<Point> rec_pos_sort;
+	vector<Point> rec_pos = get_blobs_center(*img, 5, 4000);
+
+	if (rec_pos.size() == 4)
+	{
+		vector<int> temp;
+		for (int i=0; i<rec_pos.size(); i++)
+		{
+			int xandy = rec_pos[i].x + rec_pos[i].y;
+			temp.push_back(xandy);
+		}
+		int max_index = max_element(temp.begin(), temp.end()) - temp.begin();
+		int min_index = min_element(temp.begin(), temp.end()) - temp.begin();
+		rec_pos_sort.push_back(rec_pos[min_index]);
+		rec_pos_sort.push_back(rec_pos[max_index]);
+		int r1=-1, r2=-1;
+		for (int i=0; i<4; i++)
+		{
+			if (i != min_index && i != max_index)
+			{
+				if (r1==-1)  r1 = i;
+				else  r2 = i;
+			}
+		}
+		if (rec_pos[r1].y < rec_pos[r2].y)
+		{
+			rec_pos_sort.push_back(rec_pos[r1]);
+			rec_pos_sort.push_back(rec_pos[r2]);
+		}
+		else
+		{
+			rec_pos_sort.push_back(rec_pos[r2]);
+			rec_pos_sort.push_back(rec_pos[r1]);
+		}
+		Point tempPoint = rec_pos_sort[1];
+		rec_pos_sort[1] = rec_pos_sort[3];
+		rec_pos_sort[3] = tempPoint;
+	}
+	return rec_pos_sort;
+}
+
+Point get_center_position(Mat* img_origin, Rect roi)
+{
+    Mat img(*img_origin, roi);
+    Point center_points = {0, 0}; 
+	int count = 0;
+    for (int col=0; col < img.cols; col++)
+    {
+        for (int row=0; row < img.rows; row++)
+        {
+            int R = img.at<uchar>(row, col);
+            if (R < 2)
+            {
+                center_points.y += row;
+                center_points.x += col;
+                count++;
+            }
+        }
+    }
+	if (count > 0)
+    {
+        center_points.y = int(center_points.y / count) + roi.y;
+        center_points.x = int(center_points.x / count) + roi.x;
+        // center_points.push_back(KeyPoint);
+    }
+    return center_points;
+}
+
+
+void getContours(Mat imgDil,Mat* img)
+{
+	//创建轮廓
+	vector<vector<Point>> contours;
+	//创建层级轮廓
+	//Vec4i:每个向量具有4个整数值
+	vector<Vec4i> hierarchy;
+	//找到轮廓
+	findContours(imgDil, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+	//绘制轮廓
+	//绘制所有的，符号为-1,颜色为紫色，厚度为2
+	//drawContours(img, contours, -1,Scalar(255, 0, 255),10);
+    //过滤噪声点
+	cout << "find:" << contours.size() << endl;
+	for (int i = 0; i < contours.size(); i++)
+	{
+		//先找到每个轮廓的面积
+		int area = contourArea(contours[i]);
+		//打印出面积
+		cout << area << endl;
+		//定义轮廓对象的名称
+		string objectType;
+		//角点向量，仅存储轮廓中的角点
+		vector<vector<Point>> conPoly(contours.size());
+		//存储矩形边界框向量
+		vector<Rect> boundRect(contours.size());
+		//如果面积在可提取范围内，则提取对应轮廓
+		if (area > 1000 && area < 4000)
+		{
+			//找到每个轮廓的周长
+			float peri = arcLength(contours[i], true);
+			//从轮廓中找到曲线的角点，true表示是否闭合
+			//轮廓：contours[i]中包含所有的点
+			//而角点：conPoly[i]中仅包含角点
+			//如果是矩形，将有4个，三角形则是3个
+			approxPolyDP(contours[i], conPoly[i], 0.02 * peri,true);
+			//绘制所有的，符号为-1,颜色为紫色，厚度为2
+			drawContours(*img, conPoly, i, Scalar(255, 0, 255), 2);
+			//打印出每个轮廓的角点数
+			cout << conPoly[i].size() << endl; 
+			//绘制矩形边界框，将含纳每一个独立的形状
+			boundRect[i] = boundingRect(conPoly[i]);
+			//将边界框打印在原图上
+			rectangle(*img, boundRect[i].tl(), boundRect[i].br(), Scalar(0, 255, 0), 5);
+			//定义每个轮廓的角点数
+			int objCor = (int)conPoly[i].size();
+			//三角形判断
+			if (objCor == 3)
+			{
+				objectType = "Triangle";
+			}
+			//矩形判断
+			if (objCor == 4)
+			{
+				//定义长宽比
+				float aspectRatio = (float)boundRect[i].width / (float)boundRect[i].height;
+				cout << "长宽比"<< aspectRatio << endl;
+				//正方形判断
+				if (aspectRatio > 0.95 && aspectRatio < 1.05)
+				{
+					objectType = "Square";
+				}
+				//不然即是矩形
+				else
+				{
+					objectType = "Rectangle";
+				}
+			}
+			//圆形判断
+			if (objCor >4)
+			{
+				objectType = "Circle";
+			}
+			//在形状上方写出对应的名称
+			putText(*img, objectType, { boundRect[i].x,boundRect[i].y - 5 }, FONT_HERSHEY_DUPLEX, 0.75, Scalar(0, 69, 255), 2);
+		}
+	}
+}
---
+++ tft.cpp
@@ -0,0 +1,225 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+// for spi devices
+#include <spidev_lib++.h>
+// for delay function.
+#include <chrono>
+#include <unistd.h> 
+#include <thread>
+// for signal handling
+#include <signal.h>
+#include <JetsonGPIO.h>
+// for img process
+#include "opencv2/core.hpp"
+#include "opencv2/opencv.hpp"
+
+#include <time.h>
+
+using namespace std;
+using namespace GPIO;
+using namespace cv;
+
+int screenWidth = 240; //屏幕长度
+int screenHeight = 240; //屏幕宽度
+int PinDC = 22; //GPIO.BOARD引脚模式，第18号引脚
+int PinReset = 18;  //GPIO.BOARD引脚模式，第22号引脚
+int PinLED = 12; //GPIO.BOARD引脚模式，第12号引脚
+
+spi_config_t spi_config;
+uint8_t tx_buffer[32];
+uint8_t rx_buffer[32];
+
+void LCDReset()
+{
+    // 重置电平时序
+    GPIO::output(PinReset, 1);
+    usleep(1000*200);
+    GPIO::output(PinReset, 0);
+    usleep(1000*200);
+    GPIO::output(PinReset, 1);
+    usleep(1000*200);
+}
+
+void sendCommand(SPI* spi, uint8_t* command, uint8_t bytes[], uint8_t length)
+{
+    GPIO::output(PinDC, 0);
+    spi->write(command, 1);
+    GPIO::output(PinDC, 1);
+    spi->write(bytes, length);
+    GPIO::output(PinDC, 1);
+}
+
+void sendSignalCommand(SPI* spi, uint8_t* command)
+{
+    GPIO::output(PinDC, 0);
+    // cout << strlen((char*)command) << endl;
+    spi->write(command, 1);
+}
+
+void sendManyBytes(SPI* spi, uint8_t *bytes, int length)
+{
+    GPIO::output(PinDC, 1);
+    spi->write(bytes, length);
+}
+
+void st7789_init(SPI* spi)
+{
+    GPIO::output(PinLED, 1);
+    LCDReset();
+    uint8_t command = 0x36;
+    uint8_t bytes[1] = {0x00};
+    sendCommand(spi, &command, bytes, sizeof(bytes));
+    command = 0x3A;
+    uint8_t bytes1[1] = {0x05};
+    sendCommand(spi, &command, bytes1, sizeof(bytes1));
+    command = 0xB2;
+    uint8_t bytes2[5] = {0x0C,0x0C,0x00,0x33,0x33};
+    sendCommand(spi, &command, bytes2, sizeof(bytes2));
+    command = 0xB7;
+    uint8_t bytes3[1] = {0x35};
+    sendCommand(spi, &command, bytes3, sizeof(bytes3));
+    command = 0xBB;
+    uint8_t bytes4[1] = {0x19};
+    sendCommand(spi, &command, bytes4, sizeof(bytes4));
+    command = 0xC0;
+    uint8_t bytes5[1] = {0x2C};
+    sendCommand(spi, &command, bytes5, sizeof(bytes5));
+    command = 0xC2;
+    uint8_t bytes6[1] = {0x01};
+    sendCommand(spi, &command, bytes6, sizeof(bytes6));
+    command = 0xC3;
+    uint8_t bytes7[1] = {0x12};
+    sendCommand(spi, &command, bytes7, sizeof(bytes7));
+    command = 0xC4;
+    uint8_t bytes8[1] = {0x20};
+    sendCommand(spi, &command, bytes8, sizeof(bytes8));
+    command = 0xC6;
+    uint8_t bytes9[1] = {0x0F};
+    sendCommand(spi, &command, bytes9, sizeof(bytes9));
+    command = 0xD0;
+    uint8_t bytes10[2] = {0xA4,0xA1};
+    sendCommand(spi, &command, bytes10, sizeof(bytes10));
+    command = 0xE0;
+    uint8_t bytes11[14] = {0xD0,0x04,0x0D,0x11,0x13,0x2B,0x3F,0x54,0x4C,0x18,0x0D,0x0B,0x1F,0x23};
+    sendCommand(spi, &command, bytes11, sizeof(bytes11));
+    command = 0xE1;
+    uint8_t bytes12[14] = {0xD0,0x04,0x0C,0x11,0x13,0x2C,0x3F,0x44,0x51,0x2F,0x1F,0x1F,0x20,0x23};
+    sendCommand(spi, &command, bytes12, sizeof(bytes12));
+    command = 0x21;
+    sendSignalCommand(spi, &command);
+    command = 0x11;
+    sendSignalCommand(spi, &command);
+    command = 0x29;
+    sendSignalCommand(spi, &command);
+    cout << "finish sending" << endl;
+}
+
+
+void setWindow(SPI* spi)
+{
+    uint8_t command = 0x2A;
+    sendSignalCommand(spi, &command);
+    uint8_t bytes[2] = {0>>8,0};
+    sendManyBytes(spi, bytes, sizeof(bytes));
+    uint8_t x1 = screenHeight - 1;
+    bytes[0] = x1>>8; bytes[1] = x1;
+    sendManyBytes(spi, bytes, sizeof(bytes));
+
+    command = 0x2B;
+    sendSignalCommand(spi, &command);
+    bytes[0] = 0>>8; bytes[1] = 0;
+    sendManyBytes(spi, bytes, sizeof(bytes));
+    uint8_t y1 = screenWidth - 1;
+    bytes[0] = y1>>8; bytes[1] = y1;
+    sendManyBytes(spi, bytes, sizeof(bytes));
+
+    command = 0x2C;
+    sendSignalCommand(spi, &command);
+    cout << "setWindow dnow" << endl;
+}
+
+void img2rgb565(Mat img, uint8_t* rgb565)
+{
+    int count = 0;
+    for (int x=0; x < screenWidth; x++)
+    {
+        for (int y=screenHeight-1; y>=0; y--)
+        {
+            Vec3b pixel = img.at<Vec3b>(y, x);
+            uint8_t blue = pixel[0]; 
+            uint8_t green = pixel[1]; 
+            uint8_t red = pixel[2]; 
+            red = red >> 3;
+            green = green >> 2;
+            blue = blue >> 3;
+            int highBit = 0| (red<<3) | (green>>3);
+            int lowBit  = 0 | (green << 5) | blue;
+            rgb565[count] = highBit;
+            count++;
+            rgb565[count] = lowBit;
+            count++;
+        }
+    }
+}
+
+void drawImg16BitColor(SPI* spi, Mat img)
+{
+    uint8_t bytes[115200] = {};
+    GPIO::output(PinDC, 1);
+    img2rgb565(img, bytes);
+
+    // sendManyBytes(spi, bytes, sizeof(bytes));
+    clock_t start=clock();
+    // for (int i=0; i < sizeof(bytes); i++)
+    // {
+    //     spi->write(&bytes[i], 1);
+    // }
+    int buffer_size = 255;
+    uint8_t send_buffer[255] = {}; 
+    int i = 0;
+    for (; i < (int)sizeof(bytes)/buffer_size; i++)
+    {
+        if (i*buffer_size+buffer_size > sizeof(bytes))
+        {
+            copy(bytes+i*buffer_size, bytes+sizeof(bytes)-1, send_buffer);
+
+            spi->write(send_buffer, sizeof(bytes)-i*buffer_size);
+        }
+        else{
+            copy(bytes+i*buffer_size, bytes+i*buffer_size+buffer_size, send_buffer);
+            spi->write(send_buffer, buffer_size);
+        }
+        
+    }
+    double Times=(double)(clock()-start)/CLOCKS_PER_SEC;
+    cout << Times <<"send finish" << endl;
+}
+
+
+// int main( void)
+// {
+//     GPIO::setmode(BOARD);
+//     GPIO::setwarnings(false);
+//     GPIO::setup(PinDC, GPIO::OUT);
+//     GPIO::setup(PinReset, GPIO::OUT);d
+//     GPIO::setup(PinLED, GPIO::OUT);
+//     SPI *mySPI = NULL;
+//     spi_config.mode=0;
+//     spi_config.speed=24000000;
+//     spi_config.delay=0;
+//     spi_config.bits_per_word=8;
+
+//     mySPI=new SPI("/dev/spidev0.0",&spi_config);
+//     cout << mySPI->begin() << endl;
+//     st7789_init(mySPI);
+//     setWindow(mySPI);
+
+//     // Mat img = imread("../test.jpg");
+//     Mat img = imread("../test_hsv.png");
+//     resize(img, img, Size(240, 240));
+
+//     drawImg16BitColor(mySPI, img);
+//     delete mySPI;
+//     return 1;
+// }
